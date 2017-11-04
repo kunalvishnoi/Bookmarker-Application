4aea0a188ae5e12f953c15f17af248f4a8db8610
kunalvishnoi <vishnoi.kunal0@gmail.com>
2017-11-05 01:02:08 +0530

    update

--- js.js
+++ js.js
@@ -1,38 +1,81 @@
+// Listen for form submit
+document.getElementById('myBtn').addEventListener('click', saveBookmark);
 
-document.getElementById("myBtn").addEventListener("click", displayDate);
-function displayDate(e) {
-    var siteName=document.getElementById("name").value;
-    var siteUrl=document.getElementById("url").value;
 
-var bookmark = {
-name: siteName,
-URL: siteUrl
-}
-if(localStorage.getItem('bookmarks')===null){
+function saveBookmark(e){
+  
+  var siteName =document.getElementById('siteName').value;
+  var siteUrl =document.getElementById('siteUrl').value;
 
-  var bookmarks =[];
-  bookmarks.push(bookmark);
-  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));   
-}
-  else{
-    var bookmarks= JSON.parse(localStorage.getItem('bookmarks'));
-    bookmarks.push(bookmark);
-  }
-       e.preventDefault(); 
-}
-
-      function my(){
- var bookmarks= JSON.parse(localStorage.getItem('bookmarks'));
-var results=document.getElementById('results');
-results.innerHTML ='';
-for(var i=0;i<bookmarks.length;i++){
-var name=bookmarks[i].name;
-var url=bookmarks[i].url;
-results.innerHTML+='<div class="well">'+
-'<h3>'+name+
-'<a class="btn btn-success" target="_blank" href="'+url+'">Visit</a> '
-'<a class="btn btn-danger"  href="'+url+'">Visit</a> '
-'</h3>'+'</div>' ;
-}
-       }
  
+
+  var bookmark = {
+    name: siteName,
+    url: siteUrl
+  }
+
+  
+  
+  if(localStorage.getItem('bookmarks') === null){
+  
+    var bookmarks = [];
+ 
+    bookmarks.push(bookmark);
+   
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
+   
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+   
+    bookmarks.push(bookmark);
+    
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+
+  document.getElementById('myForm').reset();
+
+
+  my();
+
+  e.preventDefault();
+}
+
+function deleteBookmark(url){
+
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // Loop throught bookmarks
+  for(var i =0;i < bookmarks.length;i++){
+    if(bookmarks[i].url == url){
+      // Remove from array
+      bookmarks.splice(i, 1);
+    }
+  }
+  
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+
+  my();
+}
+
+function my(){
+
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // Get output id
+  var bookmarksResults = document.getElementById('bookmarksResults');
+
+
+  bookmarksResults.innerHTML = '';
+  for(var i = 0; i < bookmarks.length; i++){
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    bookmarksResults.innerHTML += '<div class="well">'+
+                                  '<h3>'+name+
+                                  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
+                                  ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
+                                  '</h3>'+
+                                  '</div>';
+  }
+}
+
+
